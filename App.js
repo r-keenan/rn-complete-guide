deb6e370b59dd01435db054c7b3fd2d900554962
Ross Keenan <ross.keenan@protonmail.com>
2021-05-16 18:54:40 -0500

    Add button and textinput field

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <View></View>
+    <View style={{ paddingTop: 50 }}>
+      <View>
+        <TextInput
+          placeholder="Course Goal"
+          style={{ borderBottomColor: "black", borderBottomWidth: 1 }}
+        />
+        <Button title="ADD" />
+      </View>
       <View></View>
     </View>
   );
